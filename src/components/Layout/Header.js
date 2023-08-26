2132d8ccb96c0d61586aa63b2f9113bf88c642d8
mika2022new <halemile@yahoo.com>
2023-08-26 21:07:35 +0200

    1 commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Header.module.css';
+import sushiImage from "../../assets/sushi.jpg";
+
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+    return (
+        <React.Fragment>
+            <header className={styles.header} >
+                <h1>Yapona Kuxnya</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={styles['main-image']}>
+                <img src= {sushiImage} 
+                alt='img'/>
+
+            </div>
+            
+        </React.Fragment>
+    );
+};
+
+export default Header;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PromoText from "./PromoText";
+import MealList from './MealList';
+
+const Meals = () => {
+    return (
+        <React.Fragment>
+            <PromoText />
+            <MealList />
+            
+        </React.Fragment>
+    );
+};
+
+export default Meals;
